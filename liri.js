997cc1e8730fa6f64ed4b84e7c7dc654915bd4e3
semegoita <semere5ha@yahoo.com>
2018-05-03 14:00:47 -0700

    commads added

--- liri.js
+++ liri.js
@@ -12,11 +12,17 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
-// var inputString = process.argv[2];
-// var value = process.argv.slice(3);
+var inputString = process.argv[2];
+//var value = process.argv.slice(3);
+if (inputString = myTweets) {
+  myTweets();
+
+}
 
 
 function myTweets() {
+
+  var params = { screen_name: 'semegoita', count: 21 };
 
   client.stream('statuses/filter', { track: 'twitter' }, function (stream) {
     stream.on('data', function (tweet) {
@@ -29,35 +35,38 @@ function myTweets() {
   });
 }
 
-function spotifyThisSong(){
-spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+// function spotifyThisSong(){
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
 
-  console.log(data.tracks.items[0]);
-});
-}
+//   console.log(data.tracks.items[0]);
+// });
+// }
 
-function movieThis(){
+// function movieThis(){
 
-  // Grab the movieName which will always be the third node argument.
-  var movieName = process.argv[2];
+//   // Grab the movieName which will always be the third node argument.
+//   var movieName = process.argv[2];
 
-  // Then run a request to the OMDB API with the movie specified
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+//   // Then run a request to the OMDB API with the movie specified
+//   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-  // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
+//   // This line is just to help us debug against the actual URL.
+//   console.log(queryUrl);
 
-  request(queryUrl, function(error, response, body) {
+//   request(queryUrl, function(error, response, body) {
 
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
+//     // If the request is successful
+//     if (!error && response.statusCode === 200) {
 
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log( "The Movie title is: " + JSON.parse(body).title +"\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating of the movie: " + JSON.parse(body).IMDBrating)
-    }
-  });
-}
+//       // Parse the body of the site and recover just the imdbRating
+//       // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//       console.log( "The Movie title is: " + JSON.parse(body).title +"\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating of the movie: " + JSON.parse(body).IMDBrating)
+//     }
+//   });
+// }
+// function doWhatItSays(){
+
+// }
